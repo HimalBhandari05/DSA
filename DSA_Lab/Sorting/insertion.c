fa898e0bcf8254b46bb1fefad040674fc1ef79b5
HimalBhandari05 <himalbhandari342@gmail.com>
2025-08-03 20:54:01 +0545

    Insertion Sort using c

---
+++ DSA_Lab/Sorting/insertion.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define MAX 200000
+
+void insertion_sort(int A[] , int n){
+    
+}
+
+void display(int A[] , int n){
+    int i ;
+    for ( i =0 ; i < n ; i++){
+        printf("%d  " ,A[i]);
+    }
+    printf("\n");
+}
