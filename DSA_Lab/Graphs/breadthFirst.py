fb79881f31850e107ba4b77e9b2906020181bdad
HimalBhandari05 <himalbhandari342@gmail.com>
2025-08-19 16:20:08 +0545

    Depth First Traversal

---
+++ DSA_Lab/Graphs/breadthFirst.py
@@ -0,0 +1,24 @@
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
+def bft(graph , start):
+    queue = []
+    visited = []
+    queue.append(start)
+    while queue:
+        visited_value = queue.pop(0)
+        visited.append(visited_value)
+        for adj_vert in graph[visited_value]:
+            if adj_vert not in queue and adj_vert not in visited:
+                queue.append(adj_vert)
+        
+    return visited
+            
+        
+returned_list = bft(graph ,'A')
+print(returned_list)
